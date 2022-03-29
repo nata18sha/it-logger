9108838b86db097900da45b70d8d250bdeca4b41
nata18sha <nata18sha@gmail.com>
2022-03-29 21:49:21 +0300

    Add Technicians added

--- src/actions/techActions.js
+++ src/actions/techActions.js
@@ -14,9 +14,29 @@ export const getTechs = () => async dispatch => {
         const res = await fetch('/techs');
         const data = await res.json();
 
-        dispatch({ type: GET_TECHS , payload: data });
+        dispatch({ type: GET_TECHS, payload: data });
     } catch (error) {
-        dispatch({ type: TECHS_ERROR , payload: error.response.statusText  });
+        dispatch({ type: TECHS_ERROR, payload: error.response.statusText });
+    }
+};
+
+//Add tech
+export const addTech = tech => async dispatch => {
+    try {
+        setLoading();
+
+        const res = await fetch('/techs', {
+            method: 'POST',
+            body: JSON.stringify(tech),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        const data = await res.json();
+
+        dispatch({ type: ADD_TECH, payload: data });
+    } catch (error) {
+        dispatch({ type: TECHS_ERROR, payload: error.response.statusText });
     }
 };
 
