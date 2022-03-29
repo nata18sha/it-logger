65969258366b36fc6fec19a0e199346adbf913bf
nata18sha <nata18sha@gmail.com>
2022-03-29 20:28:36 +0300

    Search Logs added

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -7,6 +7,7 @@ import {
     SET_CURRENT,
     CLEAR_CURRENT,
     UPDATE_LOG,
+    SEARCH_LOGS,
 } from './types';
 
 //Get logs from server
@@ -73,6 +74,20 @@ export const updateLog = log => async dispatch => {
         const data = await res.json();
 
         dispatch({ type: UPDATE_LOG, payload: data });
+    } catch (error) {
+        dispatch({ type: LOGS_ERROR, payload: error.response.data });
+    }
+};
+
+//Search server logs
+export const searchLogs = text => async dispatch => {
+    try {
+        setLoading();
+
+        const res = await fetch(`/logs?q=${text}`);
+        const data = await res.json();
+
+        dispatch({ type: SEARCH_LOGS, payload: data });
     } catch (error) {
         dispatch({ type: LOGS_ERROR, payload: error.response.data });
     }
