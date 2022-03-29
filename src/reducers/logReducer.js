d9b61eaebd84fc02ca424322a699b1d5a07a820a
nata18sha <nata18sha@gmail.com>
2022-03-29 18:53:46 +0300

    Add Logs added

--- src/reducers/logReducer.js
+++ src/reducers/logReducer.js
@@ -1,4 +1,4 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR } from '../actions/types';
+import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG } from '../actions/types';
 
 const initialState = {
     logs: null,
@@ -15,6 +15,12 @@ const logReducer = (state = initialState, action) => {
                 logs: action.payload,
                 loading: false,
             };
+        case ADD_LOG:
+            return {
+                ...state,
+                logs: [...state.logs, action.payload],
+                loading: false
+            };
         case SET_LOADING:
             return {
                 ...state,
@@ -30,6 +36,5 @@ const logReducer = (state = initialState, action) => {
             return state;
     }
 };
-
 
 export default logReducer;
